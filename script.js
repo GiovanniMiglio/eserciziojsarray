0362baec45d11db45cabfd8e48abf6f2b3a8b741
GiovanniMiglio <gmiglio89@gmail.com>
2025-08-04 15:50:23 +0200

    svolto esercizio3

--- script.js
+++ script.js
@@ -12,8 +12,25 @@
 
 
 //esercizio2
-let numbers = [3, 7, -2, 5, 8, 1, 2, 5, 6, -4, 12, 25]
-numbers.sort(function(a, b){return b - a})
-console.log(numbers);
-numbers.sort(function(a, b){return a - b})
-console.log(numbers);
+// let numbers = [3, 7, -2, 5, 8, 1, 2, 5, 6, -4, 12, 25]
+// numbers.sort(function(a, b){return b - a})
+// console.log(numbers);
+// numbers.sort(function(a, b){return a - b})
+// console.log(numbers);
+
+
+//esercizio 3
+let numbers = [3, 7, 2, 5, 8, 1, 2, 5, 6, 4, 12, 25]
+function mediaNumeri(number) {
+    let somma = number.reduce((acc,n) => acc + n, 0);
+    let media = Math.floor(somma / number.length);
+    let mediamin = number.filter(n=> n < media);
+    let mediamax = number.filter(n=> n > media);
+    
+    console.log(`la media dei numeri e' ${media}`);
+    console.log(`i numeri minoria alla media sono: ${mediamin}`);
+    console.log(`i numeri maggiori alla media sono: ${mediamax}`);
+    
+}
+
+mediaNumeri(numbers)
